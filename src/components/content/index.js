0e8e1b76d714362bc43343f9df0d090e1e685c7c
Ben Harris <39844838+harrishills@users.noreply.github.com>
2018-12-06 13:51:13 -0800

    Initial commit

---
+++ src/components/content/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>My Website</h1>
+        <section>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+          </ul>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Content;
